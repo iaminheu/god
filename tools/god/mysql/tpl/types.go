614fa8f49926efd3776073640c719fb92b50e174
zs <978007000@qq.com>
2020-11-14 00:41:26 +0800

    自动忽略create_time, update_time字段；表名自动配置无需手动传参

--- tools/god/mysql/tpl/types.go
+++ tools/god/mysql/tpl/types.go
@@ -2,13 +2,13 @@ package tpl
 
 var Types = `
 type (
+	{{.table}} struct {
+		{{.fields}}
+	}
+
 	{{.table}}Model struct {
 		{{if .withCache}}sqlx.CachedConn{{else}}conn sqlx.Conn{{end}}
 		table string
-	}
-
-	{{.table}} struct {
-		{{.fields}}
 	}
 )
 `
