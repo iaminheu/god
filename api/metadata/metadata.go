1e1239289040b423f6cbeb222fef4d34226fd748
zs <978007000@qq.com>
2021-01-07 11:32:28 +0800

    增强客户端ip真实度、新增api层上下文转metadata

--- api/metadata/metadata.go
+++ api/metadata/metadata.go
@@ -18,50 +18,7 @@ func (md MD) Len() int {
 	return len(md)
 }
 
-type mdIncomingKey struct{}
-type mdOutgoingKey struct{}
-
 // NewIncomingContext creates a new context with incoming md attached.
 func NewContext(ctx context.Context, md MD) context.Context {
-	return context.WithValue(ctx, mdIncomingKey{}, md)
-}
-
-// NewIncomingContext creates a new context with incoming md attached.
-func NewIncomingContext(ctx context.Context, md MD) context.Context {
-	return context.WithValue(ctx, mdIncomingKey{}, md)
-}
-
-// NewOutgoingContext creates a new context with outgoing md attached. If used
-// in conjunction with AppendToOutgoingContext, NewOutgoingContext will
-// overwrite any previously-appended metadata.
-func NewOutgoingContext(ctx context.Context, md MD) context.Context {
-	return context.WithValue(ctx, mdOutgoingKey{}, rawMD{md: md})
-}
-
-// FromIncomingContext returns the incoming metadata in ctx if it exists.  The
-// returned MD should not be modified. Writing to it may cause races.
-// Modification should be made to copies of the returned MD.
-func FromIncomingContext(ctx context.Context) (md MD, ok bool) {
-	md, ok = ctx.Value(mdIncomingKey{}).(MD)
-	return
-}
-
-// FromOutgoingContextRaw returns the un-merged, intermediary contents
-// of rawMD. Remember to perform strings.ToLower on the keys. The returned
-// MD should not be modified. Writing to it may cause races. Modification
-// should be made to copies of the returned MD.
-//
-// This is intended for gRPC-internal use ONLY.
-func FromOutgoingContextRaw(ctx context.Context) (MD, [][]string, bool) {
-	raw, ok := ctx.Value(mdOutgoingKey{}).(rawMD)
-	if !ok {
-		return nil, nil, false
-	}
-
-	return raw.md, raw.added, true
-}
-
-type rawMD struct {
-	md    MD
-	added [][]string
+	return context.WithValue(ctx, struct{}{}, md)
 }
