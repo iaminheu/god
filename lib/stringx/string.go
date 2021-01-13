aac5ef70326b965714d5af9305a98c6a3b06feb0
zs <978007000@qq.com>
2021-01-13 15:40:12 +0800

    数据库表字段增加转义

--- lib/stringx/string.go
+++ lib/stringx/string.go
@@ -70,6 +70,23 @@ func Remove(strings []string, strs ...string) []string {
 	return out
 }
 
+func RemoveDBFields(strings []string, strs ...string) []string {
+	out := append([]string(nil), strings...)
+
+	for _, str := range strs {
+		var n int
+		for _, v := range out {
+			if v != "`"+str+"`" {
+				out[n] = v
+				n++
+			}
+		}
+		out = out[:n]
+	}
+
+	return out
+}
+
 func Reverse(s string) string {
 	runes := []rune(s)
 
--- tools/god/mysql/tpl/vars.go
+++ tools/god/mysql/tpl/vars.go
@@ -4,8 +4,8 @@ var Vars = `
 var (
 	{{.table}}FieldList          = builder.FieldList(&{{.camelTable}}{})
 	{{.table}}Fields                = strings.Join({{.table}}FieldList, ",")
-	{{.table}}FieldsAutoSet         = strings.Join(stringx.Remove({{.table}}FieldList, {{if .autoIncrement}}"{{.primaryKey}}",{{end}} "created_at", "updated_at", "create_time", "update_time"), ",")
-	{{.table}}FieldsWithPlaceHolder = strings.Join(stringx.Remove({{.table}}FieldList, "{{.primaryKey}}", "created_at", "updated_at", "create_time", "update_time"), "=?,") + "=?"
+	{{.table}}FieldsAutoSet         = strings.Join(stringx.RemoveDBFields({{.table}}FieldList, {{if .autoIncrement}}"{{.primaryKey}}",{{end}} "created_at", "updated_at", "create_time", "update_time"), ",")
+	{{.table}}FieldsWithPlaceHolder = strings.Join(stringx.RemoveDBFields({{.table}}FieldList, "{{.primaryKey}}", "created_at", "updated_at", "create_time", "update_time"), "=?,") + "=?"
 
 	{{if .withCache}}{{.cacheKeys}}{{end}}
 )
